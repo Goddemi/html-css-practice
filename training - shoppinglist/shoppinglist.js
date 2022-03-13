6bd204fa13ab5954c6457703bb283e7c59f39875
Goddemi <abraxas1225@naver.com>
2022-03-13 22:07:56 +0900

    training - shoppinglist js (1)

--- training - shoppinglist/shoppinglist.js
+++ training - shoppinglist/shoppinglist.js
@@ -3,6 +3,9 @@
 const greeting = document.querySelector(".greeting");
 const greetingInput = greeting.querySelector(".greeting-input");
 
+const shoppingWindow = document.querySelector(".shopping-window");
+const title = shoppingWindow.querySelector(".title");
+
 const HIDDEN = "hidden";
 
 
@@ -10,5 +13,40 @@ const HIDDEN = "hidden";
 greeting.addEventListener("submit", (e)=> {
     e.preventDefault();
     greeting.classList.add(HIDDEN);
+    shoppingWindow.classList.remove(HIDDEN);
+    
     const username = greetingInput.value;
+    title.innerHTML = `${username}'s shopping list`
 });
+
+
+const listForm = shoppingWindow.querySelector(".list-form");
+const listInput = listForm.querySelector("input");
+
+const listName = shoppingWindow.querySelector(".list-name");
+
+
+function listAdding()   {
+    const listValue = listInput.value;
+    listInput.value = "";
+
+    const listDiv = document.createElement("div");
+
+    const listing = document.createElement("li");
+    listing.innerHTML = listValue;
+    listing.style.listStyle = "none";
+    listDiv.appendChild(listing);
+
+    const deleting = document.createElement("div");
+    deleting.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    listDiv.appendChild(deleting);
+
+    listName.appendChild(listDiv);
+}
+
+
+
+listForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    listAdding();
+})
