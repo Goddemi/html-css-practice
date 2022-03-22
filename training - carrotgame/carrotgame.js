d4959b434efe2c6cc714bb144123231759440d27
Goddemi <abraxas1225@naver.com>
2022-03-22 23:25:07 +0900

    carrot game done 1st (before refactoring)

--- training - carrotgame/carrotgame.js
+++ training - carrotgame/carrotgame.js
@@ -1,38 +1,109 @@
 
-
-
-
 //play button
 
 const header = document.querySelector("header")
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+
 const playstopBtn = document.querySelector(".playstop__btn")
 const playIcon = document.querySelector("button i");
+const replay = document.querySelector(".replay")
+
+const bgm = new Audio("sound/bg.mp3");
+const carrotPull = new Audio("sound/carrot_pull.mp3");
+const bugPull = new Audio("sound/bug_pull.mp3");
+const gameWin = new Audio("sound/game_win.mp3");
+const gameLose = new Audio("sound/alert.wav");
+
+// timer
+
+const timer = document.querySelector(".time");
+let timeInterval = undefined;
+
+
+function timerHandler() {
+    let sec = 9;
+    let milisec = 99;
+    timeInterval = setInterval(() => {
+        milisec = --milisec;
+        timer.innerHTML = `${sec} : ${String(milisec).padStart(2,"0")}`;
+        if (milisec < 0) {
+            sec = --sec;
+            milisec = 99;
+            timer.innerHTML = `${sec} : ${String(milisec).padStart(2,"0")}`;
+        } else if (sec == 0 && milisec == 0) {
+            losingGame()
+        } 
+    }, 10);
+}
+
+
+//replay ! 
+//나중에 replay 버튼이랑 play 버튼 기능 합치기. 
+replay.addEventListener("click",() => {
+    carrots.forEach(e => e.classList.remove("hidden"));
+    carrotBugHandler();
+    stopToPlay()
+    footerHiddenBtnVis()
+    carrotCount.innerHTML = 10;
+    timer.innerHTML = "10:00";
+    started = false;
+})
+
+
+let started = false;
+let score = 0;
 
 playstopBtn.addEventListener("click",() => {
+    if (started) {
+        stopGame();
+    } else {
+        startGame();
+    }
+    started = !started;
+});
+
+
+function startGame() {
     carrotBugHandler();
     playStopChange();
-    timerHandler()
-})
+    timerHandler();
+    bgm.play();
+    bgm.currentTime = 0;
+    main.style.visibility = "visible";
+}
+
+function stopGame() {
+    losingGame()
+    playStopChange()
+
+    main.style.visibility = "hidden";
+}
 
 function playStopChange() {
     if (playIcon.classList.contains("fa-play")) {
-        playIcon.classList.remove("fa-play");
-        playIcon.classList.add("fa-stop");
+        playToStop()
         return ;
     } else if (playIcon.classList.contains("fa-stop")) {
-        playIcon.classList.remove("fa-stop");
-        playIcon.classList.add("fa-play")
+        stopToPlay()
 }};
+
+function playToStop() {
+    playIcon.classList.remove("fa-play");
+    playIcon.classList.add("fa-stop");
+}
+
+function stopToPlay() {
+    playIcon.classList.remove("fa-stop");
+    playIcon.classList.add("fa-play")
+}
 
 
 //main carrot , bug setting
 
-const main = document.querySelector("main");
-const carrots = document.querySelectorAll(".carrot");
+
+let carrots = document.querySelectorAll(".carrot");
 const bugs = document.querySelectorAll(".bug");
-
-
-
 
 const imgSize = (document.querySelector(".carrot")).clientWidth; // carrot , bug 사이즈가
 
@@ -41,11 +112,9 @@ carrots.forEach(carrot => distribution(carrot));
 bugs.forEach(bug => distribution(bug));
 }
 
-
 function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
-
 
 function distribution(e) {
     const mainWidth = main.clientWidth;
@@ -53,38 +122,75 @@ function distribution(e) {
 
     const randomWidth = randomNumber(0, mainWidth - imgSize);
     const randomHeight = randomNumber(0, mainHeight - imgSize);
-    
-
-    // const randomWidth = Math.floor(Math.random()*1300); //이 숫자를 정확하게 캐치할 수 있었으면 좋겠다. 곱해주는 숫자. 
-    // const randomHeight = Math.floor(Math.random()*150);
 
     e.style.transform= `translate(${randomWidth}px, ${randomHeight}px)`;
-
 }
 
-carrotBugHandler();
+//function WinningGame!
 
+const resultText = document.querySelector(".result__value");
+ 
+function winningGame() {
+    clearInterval(timeInterval);
+    footerVisBtnHidden()
+    resultText.innerHTML = `YOU WIN`
+    bgm.pause()
+    gameWin.play()
+}
 
+//function losingGame!
 
-// timer
+function losingGame() {
+    clearInterval(timeInterval);
+    footerVisBtnHidden()
+    resultText.innerHTML = `YOU LOSE`
+    bgm.pause()
+    gameLose.play();
+}
 
+function footerVisBtnHidden() {
+    footer.classList.remove("hidden");
+    playstopBtn.classList.add("hidden");
+}
 
-const timer = document.querySelector(".time");
-let sec = 9;
-let milisec = 99;
+function footerHiddenBtnVis() {
+    footer.classList.add("hidden")
+    playstopBtn.classList.remove("hidden");
+}
 
-function timerHandler() {
-    let timerInterval = setInterval(() => {
-        milisec = --milisec;
-        timer.innerHTML = `${sec} : ${String(milisec).padStart(2,"0")}`;
-        if (milisec < 0) {
-            sec = --sec;
-            milisec = 99;
-            timer.innerHTML = `${sec} : ${String(milisec).padStart(2,"0")}`;
-        } else if (sec == 0 && milisec == 0) {
-            clearInterval(timerInterval);
-        }
-    }, 10);
+// click disappearing
+
+main.addEventListener("click", carrotClick);
+    
+function carrotClick(e) {
+    if (!started) {
+        return;
+    } else {
+        if(e.target.className === "carrot") {
+            e.target.classList.add("hidden");
+            carrotQuantity();
+            carrotPull.play();
+            
+        } else if (e.target.className === "bug") {
+            losingGame()
+            bugPull.play();
+        };
+        
+}};
+
+// carrot quantities
+
+const carrotCount = document.querySelector(".carrot__count");
+
+function carrotQuantity() {
+    carrots = document.querySelectorAll(".carrot");
+    const carrotArray = [...carrots];
+    const lastCarrot = carrotArray.filter(e => e.className ==="carrot hidden");
+    const lastCarrotNumber = 10 - (lastCarrot.length);
+    carrotCount.innerHTML = lastCarrotNumber;
+    if (lastCarrotNumber === 0) {
+        winningGame()
+    }
 }
 
 
